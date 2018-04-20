538b05a36fcbd5ef8daf845ecaacdfbcee3bf446
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-04-20 20:39:56 +0200

    push schedule

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   version: '1.1',
   timestamp: '20/04/2018 11:30',
-  scheduleUrl: 'TEST-SCRIPT.xml',
+  scheduleUrl: 'schedule.xml',
   roomStateUrl: 'https://fosdem.loomchild.net/listrooms',
   roomStatePollInterval: 60,
   analyticsUrl: ''
