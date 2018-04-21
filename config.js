061c08fde64fd9202d8ec6162375021addc808e4
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-04-21 08:24:45 +0200

    fix for room states

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   version: '1.1.2',
   timestamp: '21/04/2018 08:00',
   scheduleUrl: 'schedule.xml',
-  roomStateUrl: 'https://fosdem.loomchild.net/listrooms',
+  roomStateUrl: 'listrooms.json',
   roomStatePollInterval: 60,
   analyticsUrl: ''
 }
