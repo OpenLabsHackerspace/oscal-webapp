d03c71144acae9d20aa513b3cd0fbb99d724eff4
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-05-08 22:06:14 +0200

    some small changes

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   version: '1.2',
-  timestamp: '07/05/2018 20:51',
+  timestamp: '08/05/2018 22:07',
   scheduleUrl: 'schedule.xml',
   roomStateUrl: 'listrooms.json',
-  roomStatePollInterval: 20,
+  roomStatePollInterval: 30,
   analyticsUrl: ''
 }
